ebe58cb68091b3ae50fb40c6f0b94658e528fe01
Vishnu K A <vishnuka141@gmail.com>
2022-10-27 16:21:21 +0530

    order_details.html

--- flipkart/order/views.py
+++ flipkart/order/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def order_list(request):
     return render(request,'order/order_list.html')
+
+def order_details(request):
+    return render(request,'order/order_details.html')
