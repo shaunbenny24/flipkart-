b1509a01de857f907e8730f55cb1f3520a80d087
shaunbenny24 <shaunchukkiriyan@gmail.com>
2022-10-31 13:40:18 +0530

    added backendf

--- flipkart/product/models.py
+++ flipkart/product/models.py
@@ -1,3 +1,42 @@
+
 from django.db import models
 
-# Create your models here.
+# Crea
+
+class Category(models.Model):
+    title = models.TextField()
+    image = models.ImageField()
+  
+
+    def _str_(self):
+        return self.title
+
+
+
+
+
+class Product(models.Model):
+    title = models.TextField()
+    companybname = models.TextField()
+    slideimage = models.ImageField()
+    slideimages = models.ImageField()
+    pimage1 = models.ImageField()
+    pimage2 = models.ImageField()
+    pimage3 = models.ImageField()
+    pimage4 = models.ImageField()
+    pimage5 = models.ImageField()
+    price = models.IntegerField() 
+    disprice =  models.IntegerField() 
+    Category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+    def _str_(self):
+        return self.title
+
+
+
+
+
+
+
+
--- flipkart/product/views.py
+++ flipkart/product/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
+from .models import Category,Product
 # Create your views here.
-def product(request):
-    return render(request,'product/productlist.html')
-def  productdescrip(request):
-    return render(request,'product/productdetail.html')
+def product(request,id=None):
+ if id:
+        category = get_object_or_404(Category,id=id)
+        descrips = Product.objects.filter(category = Category)
+
+        return render(request,'product/productlist.html',{'descrips':descrips} )
+
+ else:
+        descrips =Product.objects.all()
+        return render(request,'product/productlist.html',{'descrips':descrips}  )
+
+
+def  productdescrip(request,id):
+        productdetail = Product.objects.get(id=id)
+    
+        return render(request,'product/productdetail.html',{'productdetail':productdetail})
