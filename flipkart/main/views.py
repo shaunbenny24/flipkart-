7e4436a79e24b4a15943276553a2522d15c78860
shaunbenny24 <shaunchukkiriyan@gmail.com>
2022-11-03 15:47:25 +0530

    connected to base and added categorys

--- flipkart/main/views.py
+++ flipkart/main/views.py
@@ -1,6 +1,18 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from product.models import Category,Product
+
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+
+    products = Product.objects.all()
+    categorys = Category.objects.all()
+
+    return render(request,'index.html',{'products':products,'categorys': categorys})
+
+         
+         
+         
+            
