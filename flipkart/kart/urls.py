9c2c7df80330552c72484e5ed30d2e1fc10395a8
pathukunju <pathukunju1993@gmail.com>
2022-11-07 13:41:08 +0530

    added cart details to base and total cart page

--- flipkart/kart/urls.py
+++ flipkart/kart/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path ('cart', views.cart, name = 'cart'),
     path ('subcart/',views.subcart, name ='subcart'),
     path ('add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path ('remove/<int:product_id/',views.cart_remove),
+    path ('remove/<int:product_id/',views.cart_remove, name='cart_remove'),
     path ('cart_detail',views.cart_detail, name='cart_detail'),
     path ('subcart_detail',views.subcart_detail, name='subcart_detail')
 
--- flipkart/kart/views.py
+++ flipkart/kart/views.py
@@ -19,24 +19,29 @@ def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
-    print(form)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
-        print(cart)
+        
     return redirect('cart_detail')
 
 @require_POST
 def cart_remove(request, product_id):
+    print('sucess------------------')
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
+    
     return redirect('cart_detail')
 
 def cart_detail(request):
     cart = Cart(request)
-    # for item
-    
+    for item in  cart:
+        item['update_quantity_form'] = CartAddProductForm(initial ={
+            'quantity': item['quantity'],
+            'override': True
+        })
+        print(item)
     return render(request, 'cart/cart.html',{'cart':cart})
 def subcart_detail(request):
     cart = Cart(request)
